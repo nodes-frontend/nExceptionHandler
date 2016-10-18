0320f012f517ad0470a487c1ccc4b58149e9a1fe
Dennis Haulund Nielsen <dhni@nodes.dk>
2016-10-18 19:08:15 +0200

    fix: Final setup

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './component.module';
+import './component.config';
+import './component.provider';
+import './component.filter';
+import './component.service';
+import './component.directive';
